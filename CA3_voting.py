8c984509d32d640a9adf80a50ecda8c73fb09b4f
Sreejith Vidyadharan <sreejithvs1167@gmail.com>
2021-12-29 17:26:27 +0000

    Added dictatorship function and test

--- CA3_voting.py
+++ CA3_voting.py
@@ -32,36 +32,47 @@ def generatePreferences(values):
     # For printing a dictionary properly
     pp = pprint.PrettyPrinter(indent=4, width = 120)
 
-    print('\n\nPopulated preferences dictionary: ')
-    pp.pprint(preferences_dict)
-    print('\n\n')
+    # print('\n\nPopulated preferences dictionary: ')
+    # pp.pprint(preferences_dict)
 
     # Create a temporary dictionary to access the list index and values, as 
     temp_dict = {}
     for key, value in preferences_dict.items():
-        # temp_dict = dict(enumerate(value, start=1))     # same output for temp_dict as below loop
+        # temp_dict = dict(enumerate(value, start=1))
 
-        print(f"For agent {key}: get it's original list: \n{value}")
         for index, element in enumerate(value):
             # Creating a temp_dict, with index values of list as keys, and list elements as dictionary values
-            temp_dict[index+1] = element    # index+1 to match the alternatives start count of 1
+            temp_dict[index+1] = element
 
-        print(f'From this original list create a temporary dictionary: \n{temp_dict} \n')
+        print(temp_dict,'\n')
+
+        # To get sorted values from dictionary
+        # print(sorted(temp_dict.values())[::-1])
+
+        # To get only matching keys of sorted values from dictionary
+        # print(sorted(temp_dict, key=temp_dict.get)[::-1])
+
+        # reverse=True, for equal values, gets the key with lowest value first ([::1], sorts that issue)
+        # print(sorted(temp_dict, key=temp_dict.get, reverse=True))
 
         # Updating preferences dictionary with sorted indexes (keys) of temp_dict based on it's values
-        print(f'Get the sorted list of keys from temporary dictionary based on its values:\n {sorted(temp_dict, key=temp_dict.get)}')
-        print(f'Reverse it to get descending order:\n {sorted(temp_dict, key=temp_dict.get)[::-1]}')
         preferences_dict[key] = sorted(temp_dict, key=temp_dict.get)[::-1]
-        print("Overwrite preferences dictionary with these new index based values list for each agent(key)\n\n")
 
         # To get sorted values and matching keys as a tuple from dictionary
         # print(sorted(temp_dict.items(), key=lambda x:x[1])[::-1])
 
-    print('\n\nSorted preferences dictionary: ')
-    pprint.pprint(preferences_dict)
+    # print('\n\nSorted preferences dictionary: ')
+    # pprint.pprint(preferences_dict)
     return preferences_dict
 
 
-generatePreferences('/Users/sreejith/Downloads/voting.xlsx')
+pprint.pprint(generatePreferences('/Users/sreejith/Downloads/voting.xlsx'))
 
-# def dictatorship(preferenceProfile, agent) -> int
+def dictatorship(preferenceProfile, agent):
+     return preferenceProfile[agent][0]
+
+for agent in range(1, 9):
+    print(f'For agent {agent}: {dictatorship(generatePreferences("/Users/sreejith/Downloads/voting.xlsx"), agent)}')
+
+
+
