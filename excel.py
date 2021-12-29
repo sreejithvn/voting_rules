60e55a7a009a1a05d145880d099efa63efaeee13
Sreejith Vidyadharan <sreejithvs1167@gmail.com>
2021-12-29 17:58:40 +0000

    Separate python file for excel functionality

---
+++ excel.py
@@ -0,0 +1,33 @@
+import openpyxl
+import os
+
+# Go to folder where excel file is located
+os.chdir('/Users/sreejith/Downloads')
+print(os.getcwd())
+
+# wb = openpyxl.Workbook()
+wb = openpyxl.load_workbook('voting.xlsx')
+print(wb.sheetnames)
+
+sheet = wb['Sheet1']
+
+# wb.create_sheet('Sheet2', index=0)
+# wb.remove(sheet)
+
+print(sheet['A1'].value)
+
+# Initilizing the dictionary with agents as keys, based on max row in sheet
+preferences_dict = {key: [] for key in range(1, sheet.max_row+1)}
+# preferences_dict = {}
+print('Initialized preferences dictionary:', preferences_dict)
+
+print(sheet.max_row, sheet.max_column)
+for row in range(1, sheet.max_row+1):
+    # if row not in preferences_dict.keys():
+    #     preferences_dict[row] = []
+    for col in range(1, sheet.max_column+1):
+        preferences_dict[row].append(sheet.cell(row, col).value)
+        # print(sheet.cell(row, col).value)
+
+
+print('Populated preferences dictionary: ', preferences_dict)
