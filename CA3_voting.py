677ef1502c38a7992485db49049642fdec0a04f0
Sreejith Vidyadharan <sreejithvs1167@gmail.com>
2022-01-11 00:37:54 +0000

    Added scoringRule and STV functions

--- CA3_voting.py
+++ CA3_voting.py
@@ -1,5 +1,7 @@
 import openpyxl
 import pprint
+
+from openpyxl.descriptors.base import Length
 # import os
 
 # Go to folder where excel file is located
@@ -15,26 +17,26 @@ def generatePreferences(values):
     preferences_dict = {}
 
     for row in range(1, values.max_row+1):
-        key = row
-        if key not in preferences_dict.keys():
-            preferences_dict[key] = []
+        agent = row
+        if agent not in preferences_dict.keys():
+            preferences_dict[agent] = []
 
         # Looping through the entire column values for each row
         for col in range(1, values.max_column+1):
             # Appending each column value to the list for each row
-            preferences_dict[key].append(values.cell(row, col).value)
+            preferences_dict[agent].append(values.cell(row, col).value)
 
     
 
     # print('\n\nPopulated preferences dictionary: ')
     # pp.pprint(preferences_dict)
 
-    # Create a temporary dictionary to access the list index and values, as 
+    # Create a temporary dictionary to access the list index and values
     temp_dict = {}
-    for key, value in preferences_dict.items():
+    for agent, alternatives_list in preferences_dict.items():
         # temp_dict = dict(enumerate(value, start=1))
 
-        for index, element in enumerate(value):
+        for index, element in enumerate(alternatives_list):
             # Creating a temp_dict, with index values of list as keys, and list elements as dictionary values
             temp_dict[index+1] = element
 
@@ -49,7 +51,7 @@ def generatePreferences(values):
         # print(sorted(temp_dict, key=temp_dict.get, reverse=True))
 
         # Updating preferences dictionary with sorted indexes (keys) of temp_dict based on it's values
-        preferences_dict[key] = sorted(temp_dict, key=temp_dict.get)[::-1]
+        preferences_dict[agent] = sorted(temp_dict, key=temp_dict.get)[::-1]
 
         # To get sorted values and matching keys as a tuple from dictionary
         # print(sorted(temp_dict.items(), key=lambda x:x[1])[::-1])
@@ -85,13 +87,24 @@ def get_max_list(temp_dict):
     return list_of_keys     
 
 
-def tiebreak_output(list, tiebreak, preferences):
-    if tiebreak == 'max':
+def get_min_list(temp_dict):
+    list_of_keys = []
+    min_value = min(temp_dict.values())
+    for key, value in temp_dict.items():
+        if value == min_value:
+            list_of_keys.append(key)
+    # print(list_of_keys)
+    return list_of_keys  
+
+
+def tiebreak_output(list, tieBreak, preferences):
+    print('Checkout preferences', preferences)
+    if tieBreak == 'max':
         print(max(list))
-    elif tiebreak == 'min':
+    elif tieBreak == 'min':
         print(min(list))
     else:
-        agent = tiebreak
+        agent = tieBreak
         temp_dict = {}
         for item in list:
             temp_dict[item] = preferences[agent].index(item)
@@ -100,7 +113,29 @@ def tiebreak_output(list, tiebreak, preferences):
         print(min(temp_dict, key=temp_dict.get))
 
 
-def plurality(preferences, tiebreak):
+def scoringRule(preferences, scoreVector, tieBreak):
+    # Check if sortVector length is same as the number of alternatives
+    if len(scoreVector) != len(preferences[1]):
+        print("Incorrect input")
+        return False
+
+    # sort score vector
+    scoreVector.sort(reverse=True)
+    scoring_dict = {}
+    for alternatives_list in preferences.values():
+        for index, alternative in enumerate(alternatives_list):
+            if alternative not in scoring_dict:
+                scoring_dict[alternative] = 0
+            else:
+                scoring_dict[alternative] += scoreVector[index]
+
+    print(scoring_dict)
+    list_of_keys = get_max_list(scoring_dict)
+    print(list_of_keys)
+    return tiebreak_output(list_of_keys, tieBreak, preferences)
+
+
+def plurality(preferences, tieBreak):
     temp_dict = {}
     for key, value in preferences.items():
         temp_dict[key] = value[0]
@@ -119,10 +154,10 @@ def plurality(preferences, tiebreak):
     # print(max(temp_dict2, key=lambda key: temp_dict2[key]))
 
     list_of_keys = get_max_list(temp_dict2)
-    return tiebreak_output(list_of_keys, tiebreak, preferences)
+    return tiebreak_output(list_of_keys, tieBreak, preferences)
     
 
-def veto(preferences, tiebreak):
+def veto(preferences, tieBreak):
     # initialise preferences_veto with keys as alternatives, and with values as 0
     preferences_veto = dict.fromkeys(preferences[1], 0)
     # print(preferences_veto)
@@ -131,17 +166,17 @@ def veto(preferences, tiebreak):
     for alternative_list in preferences.values():
         # Add one to the value for every 'alternative', except for the last one
         for item in alternative_list[:-1]:
-            if item in preferences_veto.keys():   # check can be removed as all keys are already initialised and the same
-                preferences_veto[item] += 1
+            # if item in preferences_veto.keys():   # check can be removed as all keys are already initialised and the same
+            preferences_veto[item] += 1
     print(preferences_veto)
     # get list of winners (with most points)
     list_of_keys = get_max_list(preferences_veto)
     print(list_of_keys)
-    # return the winner based on the tiebreak rule
-    return tiebreak_output(list_of_keys, tiebreak, preferences)
+    # return the winner based on the tieBreak rule
+    return tiebreak_output(list_of_keys, tieBreak, preferences)
     
 
-def borda(preferences, tiebreak):
+def borda(preferences, tieBreak):
     # initialise preferences_borda with keys as agents, and with values as 0
     preferences_borda = dict.fromkeys(preferences[1], 0)
     # print(preferences_borda)
@@ -156,10 +191,10 @@ def borda(preferences, tiebreak):
     print(preferences_borda)
     list_of_keys = get_max_list(preferences_borda)
     print(list_of_keys)
-    return tiebreak_output(list_of_keys, tiebreak, preferences)
+    return tiebreak_output(list_of_keys, tieBreak, preferences)
     
 
-def harmonic(preferences, tiebreak):
+def harmonic(preferences, tieBreak):
     # initialise preferences_borda with keys as agents, and with values as 0
     preferences_harmonic = dict.fromkeys(preferences[1], 0)
     # print(preferences_harmonic)
@@ -172,8 +207,35 @@ def harmonic(preferences, tiebreak):
     print(preferences_harmonic)
     list_of_keys = get_max_list(preferences_harmonic)
     print(list_of_keys)
-    return tiebreak_output(list_of_keys, tiebreak, preferences)
-  
+    return tiebreak_output(list_of_keys, tieBreak, preferences)
+
+
+def STV(preferences, tieBreak):
+    temp_preferences = preferences
+    # temp_dict = {key: 0 for key in range(1, len(temp_preferences[1]))}
+    temp_dict = dict.fromkeys(temp_preferences[1], 0)
+    print('Initial temp dict: ', temp_dict)
+    while True:
+        for value in temp_preferences.values():
+            temp_dict[value[0]] += 1
+            print('temp_dict updated: ', temp_dict)
+        key_list = get_min_list(temp_dict)
+        print('least frequency key list: ', key_list)
+        if len(key_list) == len(temp_preferences[1]):
+            print(f'len of key list: {len(key_list)} and len of temp preferences values: {len(temp_preferences[1])}')
+            return tiebreak_output(key_list, tieBreak, preferences)
+        else:
+            # to update preferences values, by removing the least frequency keys(alternatives)
+            for value in temp_preferences.values():
+                for key in key_list:
+                    value.remove(key)
+            print(f'Updated temp preferences dict after removing key list {key_list}: \n{temp_preferences}')
+            for key in key_list:
+                temp_dict.pop(key, None)
+            print(f'Updated temp dict after removing key list {key_list}: \n{temp_dict}')
+            
+        
+
 # preferences_dictionary = {1: [4, 2, 1, 3], 
 #                           2: [4, 3, 1, 2],
 #                           3: [4, 3, 1, 2],
@@ -192,19 +254,19 @@ def harmonic(preferences, tiebreak):
 #                           7: [1, 2, 3, 4],
 #                           8: [4, 2, 1, 3]}
 
-# preferences_dictionary = {1: [4, 2, 1, 3], 
-#                           2: [4, 3, 1, 2],
-#                           3: [2, 3, 1, 4],
-#                           4: [1, 3, 4, 2],
-#                           5: [2, 3, 4, 1],
-#                           6: [2, 1, 3, 4],
-#                           7: [1, 2, 3, 4],
-#                           8: [4, 2, 5, 3]}
-
-preferences_dictionary = {1: [2, 4, 1, 3], 
+preferences_dictionary = {1: [4, 2, 1, 3], 
                           2: [4, 3, 1, 2],
                           3: [2, 3, 1, 4],
-                          }
+                          4: [1, 3, 4, 2],
+                          5: [2, 3, 4, 1],
+                          6: [2, 1, 3, 4],
+                          7: [1, 2, 3, 4],
+                          8: [4, 2, 1, 3]}
+
+# preferences_dictionary = {1: [2, 4, 1, 3], 
+#                           2: [4, 3, 1, 2],
+#                           3: [2, 3, 1, 4],
+#                           }
 
 # plurality(preferences_dictionary, 'max')
 # plurality(preferences_dictionary, 'min')
@@ -218,6 +280,20 @@ preferences_dictionary = {1: [2, 4, 1, 3],
 # borda(preferences_dictionary, 'min')
 # borda(preferences_dictionary, 1)
 
-harmonic(preferences_dictionary, 'max')
-harmonic(preferences_dictionary, 'min')
-harmonic(preferences_dictionary, 1)
+# harmonic(preferences_dictionary, 'max')
+# harmonic(preferences_dictionary, 'min')
+# harmonic(preferences_dictionary, 1)
+
+# score_vector = [0.123, 0.345, 0.543, 0.876]
+# score_vector = [0.12, 0.34, 0.54, 0.87]
+# score_vector = [1.51111, 2, 3.5, 3]
+
+# scoringRule(preferences_dictionary, score_vector, 'max')
+# scoringRule(preferences_dictionary, score_vector, 'min')
+# scoringRule(preferences_dictionary, score_vector, 1)
+
+
+# STV(preferences_dictionary, 'max')
+# STV(preferences_dictionary, 'min')
+# STV(preferences_dictionary, 1)
+
